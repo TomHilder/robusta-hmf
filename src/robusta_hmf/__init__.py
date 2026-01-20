24827db2baa8fef0c4f6a7e1b51fb6b0264fc278
Tom Hilder <tom.hilder.dlhp@gmail.com>
2026-01-21 08:24:35 +1100

    load and save state accessible to user

--- src/robusta_hmf/__init__.py
+++ src/robusta_hmf/__init__.py
@@ -3,9 +3,12 @@
 from .convergence import ConvergenceTester
 from .initialisation import Initialiser
 from .main import Robusta
+from .state import load_state_from_npz, save_state_to_npz
 
 __all__ = [
     "ConvergenceTester",
     "Initialiser",
     "Robusta",
+    "load_state_from_npz",
+    "save_state_to_npz",
 ]
